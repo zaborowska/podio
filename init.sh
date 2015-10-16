faa9ac47c9591f74a8ed061a0b9291995e1b68c0
Anna <anna.zaborowska@cern.ch>
2015-10-16 15:11:05 +0200

    Preparation of init and cmake files

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.02.08/x86_64-slc6-gcc48-opt/root/
 export CMAKE_PREFIX_PATH=/afs/cern.ch/sw/lcg/releases/gtest/1.7.0-4f83b/x86_64-slc6-gcc48-opt/
 
 export PODIO=$PWD/install
-export LD_LIBRARY_PATH=$PODIO/tests:$PODIO/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=$PODIO/tests:$PYTHONPATH
+export LD_LIBRARY_PATH=$PODIO/fastsim:$PODIO/tests:$PODIO/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=$PODIO/fastsim:$PODIO/tests:$PYTHONPATH
